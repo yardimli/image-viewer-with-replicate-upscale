aef142da626a63eba6e7b6b149665fb886122132
Ekim Emre Yardimli <ekimyardimli@izlenim.com>
2024-05-14 22:45:46 +0800

    use modal for image viewing

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,50 +1,73 @@
 $(document).ready(function () {
-	$('.update-note').click(function () {
-		var imageId = $(this).data('image-id');
-		var notes = $(this).prev('.card-text').find('.image-note').val();
+	$('.click-to-enlarge').click(function () {
+		var imageUrl = $(this).attr('data-hover-src');
+		var notes = $(this).attr('data-notes');
+		var imageId = $(this).attr('data-image-id');
+		var originalImageUrl = $(this).attr('data-image-url');
+		
+		console.log(imageUrl, notes, imageId, originalImageUrl);
+		
+		$('#modalImage').attr('src', imageUrl);
+		$('.image-note-modal').val(notes);
+		$('.image-note-modal').attr('data-image-id', imageId);
+		
+		$('.update-note-modal').attr('data-image-id', imageId);
+		$('.upscale-image-modal').attr('data-image-id', imageId)
+		$('.upscale-image-modal').attr('data-image-url', originalImageUrl);
+		
+		// Set the upscale result
+		var upscaleResultHtml = $('#upscale_result_' + imageId).html();
+		$('#upscale_result_modal').html(upscaleResultHtml);
+		$("#add-note-results").html('');
+		
+		$('#imageModal').modal('show');
+	});
+	
+	$('#imageModal').on('click', '.update-note-modal', function () {
+		var imageId = $(this).attr('data-image-id');
+		var notes = $('.image-note-modal').val();
 		$.ajax({
 			url: '/images/' + imageId + '/update-notes',
 			method: 'POST',
-			data: {
-				"_token": csrf_token,
-				notes: notes
-			},
+			data: {"_token": csrf_token, notes: notes},
 			success: function (response) {
-				alert('Notes updated successfully!');
+				$("#add-note-results").html('Notes updated successfully!');
+				$('#upscale_notes_' + imageId + '').html(notes);
 			}
 		});
 	});
 	
-	$('.upscale-image').click(function () {
-		var imageId = $(this).data('image-id');
-		var imageUrl = $(this).data('image-url');
-		$('#upscale_result_' + imageId).html('Starting Upscaling... ');
+	
+	$('#imageModal').on('click', '.upscale-image-modal', function () {
+		var imageId = $(this).attr('data-image-id');
+		var imageUrl = $(this).attr('data-image-url');
+		console.log('start upscaling', imageId, imageUrl);
+		
+		$('#upscale_result_modal').html('Starting Upscaling... ');
+		$('#upscale_result_' + imageId).html('Upscaling in progress... ');
+		
 		$.ajax({
 			url: '/images/' + imageId + '/upscale',
 			method: 'POST',
-			data: {
-				"_token": csrf_token,
-				"image_url": imageUrl
-			},
+			data: {"_token": csrf_token, "image_url": imageUrl},
 			success: function (response) {
+				$('#upscale_result_modal').html('Upscaling in progress... ');
 				$('#upscale_result_' + imageId).html('Upscaling in progress... ');
 				console.log(response.upscale_result); // You might want to do something with the result
+				
 				// Start polling for status
-				var checkStatus = function() {
+				var checkStatus = function () {
 					$.ajax({
 						url: '/images/' + imageId + '/upscale-status/' + response.prediction_id,
 						method: 'GET',
-						success: function(statusResponse) {
+						success: function (statusResponse) {
 							if (statusResponse.message === 'Image upscaled successfully.') {
-								// alert('Upscaling Complete!');
 								clearInterval(statusInterval); // Stop polling
-								// Update the UI here, e.g., displaying the upscaled image link
-								$('#upscale_result_' + imageId).html('<a href="' + statusResponse.upscale_result + '" target="_blank">View Upscaled Image</a>');
+								var upscaleResult = '<a href="' + statusResponse.upscale_result + '" target="_blank">View Upscaled Image</a>';
+								$('#upscale_result_' + imageId).html(upscaleResult);
 							} else if (statusResponse.message === 'Upscale in progress.') {
-								$('#upscale_result_' + imageId).html('Upscaling in progress... '+statusResponse.status);
-								
-							}
-							else if (statusResponse.message === 'Image upscale failed.') {
+								$('#upscale_result_' + imageId).html('Upscaling in progress... ' + statusResponse.status);
+							} else if (statusResponse.message === 'Image upscale failed.') {
 								alert('Upscaling Failed');
 								clearInterval(statusInterval); // Stop polling
 							}
@@ -53,68 +76,9 @@ $(document).ready(function () {
 				};
 				
 				var statusInterval = setInterval(checkStatus, 5000);
-				
 			}
 		});
 	});
 	
-	var LoadedImageSrc = '';
-	
-	// Mouse enter event on the card image
-	$('.card img').mouseenter(function () {
-		var hoverSrc = $(this).data('hover-src');
-		var cardOffset = $(this).closest('.card').offset();
-		var cardWidth = $(this).closest('.card').outerWidth();
-		var windowWidth = $(window).width();
-		var floatingImgWidth = 700; // Assuming a fixed floating image width
-		
-		if (hoverSrc === LoadedImageSrc) {
-			return;
-		}
-		LoadedImageSrc = hoverSrc;
-		$('#floating-image').attr('src', '');
-		
-		
-		// If there's enough space on the right side of the card, display the image there, otherwise to the left.
-		if (windowWidth - (cardOffset.left + cardWidth) > floatingImgWidth) {
-			// Display to the right
-			$('#floating-image-container').css({
-				left: cardOffset.left + cardWidth + 10, // 10px gap from the card
-				top:  $(window).scrollTop() + 10
-			});
-		} else {
-			// Display to the left (ensure it does not go off-screen)
-			var leftPosition = cardOffset.left - floatingImgWidth - 10; // 10px gap from the card
-			if (leftPosition < 0) {
-				leftPosition = 10; // Fallback to display it at the start of the window if it goes off-screen
-			}
-			$('#floating-image-container').css({
-				left: leftPosition,
-				top: $(window).scrollTop() + 10
-			});
-		}
-		
-		// Set the source of the floating image and display it
-		$('#floating-image').attr('src', hoverSrc);
-		$('#floating-image-container').show();
-	}).mouseleave(function () {
-		// Hide the floating image when mouse leaves the card image
-		LoadedImageSrc = '';
-		$('#floating-image-container').hide();
-	});
-	
-	// Additional event to hide the floating image when mouse enters and leaves the floating container (for safety)
-	$('#floating-image-container').mouseenter(function () {
-		$(this).show(); // Keep showing the floating image when over it
-	}).mouseleave(function () {
-		LoadedImageSrc = '';
-		$(this).hide(); // Hide when leaving the floating image
-	});
-	
-	//hide the floating image when user scrolls
-	$(window).scroll(function () {
-		$('#floating-image-container').hide();
-		LoadedImageSrc = '';
-	});
 	
 });
--- resources/views/images/index.blade.php
+++ resources/views/images/index.blade.php
@@ -1,10 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-	<div id="floating-image-container" style="display: none; position: absolute; z-index: 1050;">
-		<img id="floating-image" src="" alt="Hovered Image" style="width: 700px; height:700px; max-width: 700px; max-height: 700px;">
-	</div>
-	
 	<div class="container">
 		<h1 class="text-center mb-4">Image Gallery</h1>
 		<a href="{{ route('images.scan') }}" class="btn btn-primary">Rescan Storage Folder</a>
@@ -20,21 +16,23 @@
 						</div>
 						<div class="card-body">
 							<h6 class="card-title"></h6>
-							<img src="{{ route('image.display', ['my_image' => $image->id, 'width' => 300]) }}"
+							<img style="cursor: pointer;" src="{{ route('image.display', ['my_image' => $image->id, 'width' => 300]) }}"
 							     data-hover-src="{{ route('image.display', ['my_image' => $image->id, 'width' => 900]) }}"
-							     class="img-fluid mb-2" alt="{{ $image->notes }}" style="width: 100%; min-height: 200px;">
+							     class="img-fluid mb-2 click-to-enlarge" alt="{{ $image->notes }}"
+							     style="width: 100%; min-height: 200px;" data-image-id="{{ $image->id }}"
+							     data-image-url="{{ env('APP_URL') . '/' . ('storage/images/'.$image->folder.'/'.$image->image_name) }}"
+							     data-notes="{{ $image->notes }}">
 							
 							<p class="card-text">
-								<input type="text" class="form-control image-note" data-image-id="{{ $image->id }}"
-								       value="{{ $image->notes }}">
+							<div class="upscale-result" id="upscale_notes_{{ $image->id }}">
+								{{ $image->notes }}
+							</div>
 							<div class="upscale-result" id="upscale_result_{{ $image->id }}">
 								@if(!empty($image->upscale_name))
 									<a href="{{ asset('storage/upscaled/'.$image->upscale_name) }}" target="_blank">View Upscaled Image</a>
 								@endif
 							</div>
 							</p>
-							<button class="btn btn-success update-note" data-image-id="{{ $image->id }}">Update Notes</button>
-							<button class="btn btn-info upscale-image" data-image-id="{{ $image->id }}" data-image-url="{{ env('APP_URL') . '/' . ('storage/images/'.$image->folder.'/'.$image->image_name) }}">Upscale Image</button>
 						</div>
 					</div>
 				</div>
@@ -44,6 +42,28 @@
 		<div class="row">
 			<div class="col-12">
 				{{ $images->links('pagination::bootstrap-5')  }}
+			</div>
+		</div>
+	</div>
+	
+	<!-- Modal for displaying larger image and buttons -->
+	<div class="modal fade" id="imageModal" tabindex="-1" aria-labelledby="imageModalLabel" aria-hidden="true" >
+		<div class="modal-dialog modal-lg" style="max-width: 700px;">
+			<div class="modal-content">
+{{--				<div class="modal-header">--}}
+{{--					<h5 class="modal-title" id="imageModalLabel">Image Details</h5>--}}
+{{--					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>--}}
+{{--				</div>--}}
+				<div class="modal-body text-center">
+					<img id="modalImage" src="" alt="Selected Image" class="img-fluid mb-2" style="max-width: 100%; height:auto;">
+					<div id="add-note-results"></div>
+					<div class="upscale-result-modal" id="upscale_result_modal"></div>
+				</div>
+				<div class="modal-footer">
+					<input type="text" class="form-control image-note-modal" data-image-id="" value="" style="max-width: 250px; display: inline-block;">
+					<button class="btn btn-success update-note-modal" data-image-id="">Update Notes</button>
+					<button class="btn btn-info upscale-image-modal" data-image-id="" data-image-url="">Upscale Image</button>
+				</div>
 			</div>
 		</div>
 	</div>
